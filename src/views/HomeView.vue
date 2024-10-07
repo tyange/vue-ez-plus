9f10a07adaa08f872bd35f97614581c87ad29d21
tyange <usun16@gmail.com>
2024-10-07 23:21:11 +0900

    이미지 확대 기능 구현

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,89 +1,62 @@
 <script setup lang="ts">
-import { ref, onMounted, useTemplateRef } from 'vue'
+import { useTemplateRef } from 'vue'
 
-const images = ref<
-  {
-    id: string
-    author: string
-    width: number
-    height: number
-    url: string
-    download_url: string
-  }[]
->([])
+import lowImage from "../assets/HI_600_400.png"
 
-const squareElement = useTemplateRef('square-element')
-const bigImageElement = useTemplateRef('big-image')
+const squareElement = useTemplateRef<HTMLDivElement>('square-element')
+const imageElement = useTemplateRef<HTMLDivElement>('image')
+const originImageElement = useTemplateRef<HTMLDivElement>('origin-image')
 
 const update = (e: MouseEvent) => {
-  if (!squareElement.value || !bigImageElement.value) {
+  if (!squareElement.value || !imageElement.value || !originImageElement.value) {
     return
   }
 
-  const rect = bigImageElement.value.getBoundingClientRect()
-  const squareSize = 50 // 네모의 크기
+  const rect = imageElement.value.getBoundingClientRect()
 
-  // 마우스 위치를 big-image 요소 내부로 제한
-  const x = Math.min(Math.max(e.clientX - rect.left, squareSize / 2), rect.width - squareSize / 2)
-  const y = Math.min(Math.max(e.clientY - rect.top, squareSize / 2), rect.height - squareSize / 2)
+  // 마우스 위치를 image 요소 내부로 제한
+  const x = Math.min(Math.max(e.clientX - rect.left, 0), rect.width)
+  const y = Math.min(Math.max(e.clientY - rect.top, 0), rect.height)
 
+  // 네모의 위치 업데이트
   squareElement.value.style.left = `${x}px`
   squareElement.value.style.top = `${y}px`
+
+  // 큰 이미지의 배경 위치 계산
+  const ratioX = x / rect.width
+  const ratioY = y / rect.height
+
+  const bigImageX = ratioX * (1024 - 480) // 1024는 큰 이미지의 너비, 480은 보여줄 영역의 너비
+  const bigImageY = ratioY * (720 - 362) // 720은 큰 이미지의 높이, 362는 보여줄 영역의 높이
+
+  // 큰 이미지의 배경 위치 업데이트
+  originImageElement.value.style.backgroundPosition = `-${bigImageX}px -${bigImageY}px`
 }
-
-async function fetchImages() {
-  try {
-    const res = await fetch('https://picsum.photos/v2/list')
-    const data = await res.json()
-
-    images.value = data
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-onMounted(() => {
-  fetchImages()
-})
 </script>
 
 <template>
-  <main>
-    <div class="big-image" v-if="images.length > 0" @mousemove="update" ref="big-image">
-      <img :src="images[0].download_url" />
+  <main class="main">
+    <div class="image" @mousemove="update" ref="image">
+      <img :src="lowImage" alt="Low resolution image" />
       <div class="follower" ref="square-element"></div>
     </div>
-    <div>
-      <ul class="list">
-        <li class="item" v-for="image in images" :key="image.id">
-          <img :src="image.download_url" />
-        </li>
-      </ul>
+    <div class="origin-image" ref="origin-image">
     </div>
   </main>
 </template>
 
 <style scoped>
-.big-image {
+.main {
+  display: flex;
+  gap: 10rem;
+}
+.image {
   position: relative;
-  width: 30rem;
+  width: 600px;
+  height: 400px;
   overflow: hidden;
 }
-.big-image > img {
-  width: 100%;
-  height: 50%;
-}
-.list {
-  display: grid;
-  list-style: none;
-  grid-template-columns: repeat(6, 1fr);
-  gap: 1rem;
-}
-.item {
-  width: 4rem;
-  height: 4rem;
-}
-.item > img {
+.image > img {
   width: 100%;
   height: 100%;
 }
@@ -102,5 +75,12 @@ onMounted(() => {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+.origin-image {
+  width: 480px;
+  height: 362px;
+  overflow: hidden;
+  background: url(../assets/HI_1024_720.png) no-repeat 0 0;
+  background-size: 1024px 720px;
 }
 </style>

c4fea59356b8f020030dbefc1accfb02a411ddbd
tyange <usun16@gmail.com>
2024-10-07 17:26:01 +0900

    기본적인 마크업 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, useTemplateRef } from 'vue'
 
 const images = ref<
   {
@@ -11,6 +11,25 @@ const images = ref<
     download_url: string
   }[]
 >([])
+
+const squareElement = useTemplateRef('square-element')
+const bigImageElement = useTemplateRef('big-image')
+
+const update = (e: MouseEvent) => {
+  if (!squareElement.value || !bigImageElement.value) {
+    return
+  }
+
+  const rect = bigImageElement.value.getBoundingClientRect()
+  const squareSize = 50 // 네모의 크기
+
+  // 마우스 위치를 big-image 요소 내부로 제한
+  const x = Math.min(Math.max(e.clientX - rect.left, squareSize / 2), rect.width - squareSize / 2)
+  const y = Math.min(Math.max(e.clientY - rect.top, squareSize / 2), rect.height - squareSize / 2)
+
+  squareElement.value.style.left = `${x}px`
+  squareElement.value.style.top = `${y}px`
+}
 
 async function fetchImages() {
   try {
@@ -30,8 +49,9 @@ onMounted(() => {
 
 <template>
   <main>
-    <div class="big-image" v-if="images.length > 0">
+    <div class="big-image" v-if="images.length > 0" @mousemove="update" ref="big-image">
       <img :src="images[0].download_url" />
+      <div class="follower" ref="square-element"></div>
     </div>
     <div>
       <ul class="list">
@@ -45,10 +65,12 @@ onMounted(() => {
 
 <style scoped>
 .big-image {
-  width: fit-content;
+  position: relative;
+  width: 30rem;
+  overflow: hidden;
 }
 .big-image > img {
-  width: 50%;
+  width: 100%;
   height: 50%;
 }
 .list {
@@ -64,5 +86,21 @@ onMounted(() => {
 .item > img {
   width: 100%;
   height: 100%;
+}
+.follower {
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  background-color: rgba(255, 0, 0, 0.5);
+  pointer-events: none;
+  cursor: none;
+  transform: translate(-50%, -50%);
+}
+.follower::after {
+  content: '';
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
